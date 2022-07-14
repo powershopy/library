04cd3d8d1294ff5ccb7bf6274598487c1b463cf2
levilu <>
2022-07-14 16:56:41 +0800

    增加几个时间类型

--- utils/time.go
+++ utils/time.go
@@ -3,6 +3,10 @@ package utils
 import "time"
 
 const TimeFormat = "2006-01-02 15:04:05"
+const StatisticTimeFormat = "2006/01/02 15:04:05"
+const DateFormat = "2006-01-02"
+const StatisticDateFormat = "2006/01/02"
+const HourFormat = "15:04:05"
 
 func FormatTime(t time.Time, layout string) string {
 	return t.Format(layout)
