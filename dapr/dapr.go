b02f11491f01f8c9d9c69cecd58bf167070f5460
luli <luli@empowershopy.com>
2024-07-22 17:52:32 +0800

    增大ping间隔

--- dapr/dapr.go
+++ dapr/dapr.go
@@ -45,9 +45,9 @@ func init() {
 		}
 		address := net.JoinHostPort("127.0.0.1", port)
 		var kacp = keepalive.ClientParameters{
-			Time:                5 * time.Second, // send pings every 10 seconds if there is no activity
-			Timeout:             time.Second,     // wait 1 second for ping ack before considering the connection dead
-			PermitWithoutStream: true,            // send pings even without active streams
+			Time:                10 * time.Second, // send pings every 10 seconds if there is no activity
+			Timeout:             time.Second,      // wait 1 second for ping ack before considering the connection dead
+			PermitWithoutStream: true,             // send pings even without active streams
 		}
 		conn, err := grpc.DialContext(
 			ctx,
